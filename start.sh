b9731868e6f0476c007bcacfdab07cdf9ebabe7d
Manoj Phuyal <64928088+man05jp@users.noreply.github.com>
2025-08-10 15:46:12 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Start rclone HTTP server for OneDrive (reads config from /config/rclone/rclone.conf)
-rclone serve http onedrive: --addr :8080 --dir-cache-time 12h --vfs-cache-mode writes &
-
-# Start Plex
+rclone mount onedrive: /mnt/onedrive --allow-other --vfs-cache-mode writes &
 /usr/lib/plexmediaserver/Plex\ Media\ Server
